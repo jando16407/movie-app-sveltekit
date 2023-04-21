902778a46ad81cd126aa322f4d8c227410fd701e
Your Name <you@example.com>
2023-04-20 20:28:41 -0400

    update

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,3 @@
-/** @type {import('@sveltejs/adapter-vercel').Config} */
-export const config = {
-    runtime: 'edge'
-};
-
 export async function load({ fetch }) {
     const res = await fetch(
         `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API}&language=en-US&page=1`
